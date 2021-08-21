c94bf336ba256644924157c2999b1b99dee496e6
Leo-paul <leopaulmartin@hotmail.fr>
2021-08-22 00:18:14 +0200

    html skeleton

--- frontendmentor-todo-app-main/src/app/mock-todos.ts
+++ frontendmentor-todo-app-main/src/app/mock-todos.ts
@@ -1,14 +1,14 @@
 import { Todo } from './todo';
 
 export const TODOS: Todo[] = [
-  { id: 11, name: 'delectus aut autem' },
-  { id: 12, name: 'quis ut nam facilis et officia qui' },
-  { id: 13, name: 'fugiat veniam minus' },
-  { id: 14, name: 'qui ullam ratione quibusdam voluptatem quia omnis' },
-  { id: 15, name: 'illo expedita consequatur quia in' },
-  { id: 16, name: 'quo adipisci enim quam ut ab' },
-  { id: 17, name: 'molestiae perspiciatis ipsa' },
-  { id: 18, name: 'illo est ratione doloremque quia maiores aut' },
-  { id: 19, name: 'vero rerum temporibus dolor' },
-  { id: 20, name: 'ipsa repellendus fugit nisi' }
+  { id: 11, name: 'delectus aut autem' , complete: false},
+  { id: 12, name: 'quis ut nam facilis et officia qui' , complete: false },
+  { id: 13, name: 'fugiat veniam minus' , complete: false },
+  { id: 14, name: 'qui ullam ratione quibusdam voluptatem quia omnis' , complete: false },
+  { id: 15, name: 'illo expedita consequatur quia in'  , complete: false},
+  { id: 16, name: 'quo adipisci enim quam ut ab'  , complete: false},
+  { id: 17, name: 'molestiae perspiciatis ipsa'  , complete: false},
+  { id: 18, name: 'illo est ratione doloremque quia maiores aut' , complete: false },
+  { id: 19, name: 'vero rerum temporibus dolor'  , complete: false},
+  { id: 20, name: 'ipsa repellendus fugit nisi' , complete: false }
 ];
