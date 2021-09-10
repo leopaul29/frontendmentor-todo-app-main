37206787d76b19d19ddf7e1fb925c5e3dd5261ba
Leo-paul <leopaulmartin@hotmail.fr>
2021-09-10 23:44:07 +0200

    add darkmode component

--- frontendmentor-todo-app-main/src/app/app.module.ts
+++ frontendmentor-todo-app-main/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { DarkModeToggleComponent } from './dark-mode-toggle/dark-mode-toggle.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent],
+  declarations: [AppComponent, TodoListComponent, DarkModeToggleComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
