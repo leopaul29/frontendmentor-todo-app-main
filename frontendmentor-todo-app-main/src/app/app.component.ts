b14a9518dc88b9e4343ac3f66a2eeec2f8bd72cb
Leo-paul <leopaulmartin@hotmail.fr>
2021-09-15 23:06:39 +0200

    style: app darkmode

--- frontendmentor-todo-app-main/src/app/app.component.ts
+++ frontendmentor-todo-app-main/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DarkmodeService } from './darkmode.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'frontendmentor-todo-app-main';
+export class AppComponent implements OnInit {
+  constructor(private darkmodeService: DarkmodeService) {}
+
+  ngOnInit(): void {}
+
+  getDarkmodeValue() {
+    return this.darkmodeService.darkmodeValue;
+  }
 }
