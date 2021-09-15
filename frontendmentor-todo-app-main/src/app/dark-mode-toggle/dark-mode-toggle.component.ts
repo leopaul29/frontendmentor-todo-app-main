b229087106d2b17e4c8fa7cf79159084843d44eb
Leo-paul <leopaulmartin@hotmail.fr>
2021-09-15 23:09:17 +0200

    style: set darkmode button

--- frontendmentor-todo-app-main/src/app/dark-mode-toggle/dark-mode-toggle.component.ts
+++ frontendmentor-todo-app-main/src/app/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DarkmodeService } from '../darkmode.service';
 
 @Component({
   selector: 'app-dark-mode-toggle',
   templateUrl: './dark-mode-toggle.component.html',
-  styleUrls: ['./dark-mode-toggle.component.css']
+  styleUrls: ['./dark-mode-toggle.component.css'],
 })
 export class DarkModeToggleComponent implements OnInit {
+  constructor(private darkmodeService: DarkmodeService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getDarkmodeValue() {
+    return this.darkmodeService.darkmodeValue;
   }
 
+  toggleDarkmode(): void {
+    this.darkmodeService.toggleDarkmode();
+  }
 }
